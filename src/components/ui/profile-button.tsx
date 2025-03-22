a5d1a0bf55e3a4e5e62c5d4435decd6e24c6abba
adrianabatistaamicci <adriana.batista@amicci.com.br>
2025-03-22 20:39:20 -0300

    Update profile-button.tsx

--- src/components/ui/profile-button.tsx
+++ src/components/ui/profile-button.tsx
@@ -75,7 +75,7 @@ const ProfileButton = React.forwardRef<HTMLButtonElement, ProfileButtonProps>(
         type="button"
         {...props}
       >
-        <div className="flex items-center w-full px-4 py-2.5">
+        <div className="flex items-center w-full px-4 py4">
           {showClientLogo && (
             <div className="flex items-center mr-6">
               {logoSrc ? (
