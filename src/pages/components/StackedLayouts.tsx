4975946f42fe69b25bdc27ec6ee32079e1b1df0a
adrianabatistaamicci <adriana.batista@amicci.com.br>
2025-03-29 00:58:39 -0300

    Update StackedLayouts.tsx

--- src/pages/components/StackedLayouts.tsx
+++ src/pages/components/StackedLayouts.tsx
@@ -1,3 +1,10 @@
+/**
+ * @protected
+ * ATENÇÃO: Este arquivo contém conteúdo finalizado e aprovado.
+ * Não deve ser alterado diretamente pelo assistente AI.
+ * Apenas atualizações de componentes devem ser refletidas.
+ */
+
 import React, { useState } from 'react';
 import Header from '@/components/library-components/Header';
 import { Link } from 'react-router-dom';
